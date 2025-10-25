68ed253f0e4d7da922939a8227cd4fc33dbefd61
Jessica <jessicaaa11224@gmail.com>
2025-10-25 20:36:09 -0300

    actualizacion registrarse

--- registrarse/registrarse.js
+++ registrarse/registrarse.js
@@ -16,32 +16,34 @@ form.addEventListener('submit', function (e) {
         nombre: document.getElementById('username').value,
         email: document.getElementById('email').value,
         password: document.getElementById('password').value,
-        mostrarDatos: incluirDatos.value,
+        mostrarDatos: incluirDatos,
         direccion: direccion.value,
         telefono: telefono.value
     };
 
     console.log("Datos enviados al backend:", datosFormulario);
 
-    fetch('https://petsoulbackend.ngrok.app/api/auth/registerusuarios/registrar', {
+    fetch('https://petsoulbackend.ngrok.app/api/auth/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(datosFormulario)
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error al crear usuario');
-        }
-        return response.json();
-    })
-    .then(data => {
-        alert('Usuario creado exitosamente');
-        // Limpiar los campos del formulario
-        form.reset();
-    })
-    .catch(error => {
-        alert('Hubo un error: ' + error.message);
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al crear usuario');
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert('Usuario creado exitosamente');
+            // Limpiar los campos del formulario
+            form.reset();
+
+            window.location.href = '../index.html'; 
+        })
+        .catch(error => {
+            alert('Hubo un error: ' + error.message);
+        });
 });
